7b44bccb9d20b17da31b90bb6b7077d4c1c5b676
Ashish <ashish.bec1234@gmail.com>
2021-02-24 15:56:40 +0530

    Secind commit

--- reverseArray.js
+++ reverseArray.js
@@ -1,3 +1,5 @@
+
+// For Reveresd Array
 function Reversed(arr){
      let newArray = [];
    for (var i = arr.length - 1; i >= 0; i--) {
@@ -7,5 +9,24 @@ return newArray;
  }
 
  Reversed(arr);
-
+ //  arr = Array
  // Aruguement takes An Array
+//////////////// END //////////////////////////////////
+ //////////////////////////////////////////////////////
+
+ // For MIN AND MAX VALUE IN ARRAY
+
+
+ function MinandMax(arr){
+     
+    const Unique = [...new Set(arr)];
+        Unique.sort((a,b) => a-b);
+        const min = Unique[0]
+        const max = Unique[Unique.length-1]
+    return  {Values:{MaxValue:max,Min:min}}
+}
+
+MinandMax(arr)
+
+///////  arr = Array  
+////////////////  End/////////
