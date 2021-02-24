11bb496068049ef1696fd058bb6aa287e42d6cac
Ashish <ashish.bec1234@gmail.com>
2021-02-24 15:35:57 +0530

    first commit

---
+++ reverseArray.js
@@ -0,0 +1,11 @@
+function Reversed(arr){
+     let newArray = [];
+   for (var i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+}  
+return newArray;
+ }
+
+ Reversed(arr);
+
+ // Aruguement takes An Array
